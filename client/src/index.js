6538c5297608206305c6a65c3d9207826055427c
raffiner12 <1soigne2@gmail.com>
2023-03-19 19:44:27 +0900

    집에서 커밋, redux

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { Provider } from 'react-redux';  
 import 'antd/dist/antd';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
+
+const createStroeWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore) // redux에서 가져와진다.
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider
+      store = {createStroeWithMiddleware(Reducer,
+          window.__REDUX_DEVTOOLS_EXTENSION__&&
+          window.__REDUX_DEVTOOLS_EXTENSION__() // 내 어플리케이션에 Redux Devtools 연결함.
+        )}
+    >
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
